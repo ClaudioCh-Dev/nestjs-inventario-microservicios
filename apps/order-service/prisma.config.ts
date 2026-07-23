ff621eeefa7481dd3b5f2f09809f7bf38e273e91
Angelo <rideralucar@gmail.com>
2026-07-23 11:05:16 -0500

    end config prima on order-service

--- apps/order-service/prisma.config.ts
+++ apps/order-service/prisma.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'prisma/config';
+import 'dotenv/config'; // Carga las variables de tu archivo .env al process.env
 
 export default defineConfig({
   schema: 'src/database/prisma/schema.prisma',
   migrations: {
-    path: 'src/database/prisma/migrations',
+    path: 'src/database/migrations',
   },
   datasource: {
+    // Ahora process.env.DATABASE_URL sí tendrá el valor correcto
     url: process.env['DATABASE_URL'],
   },
 });
