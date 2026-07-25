566aa3279294b9d3c6e2ea539abd653f0291086e
Angelo <rideralucar@gmail.com>
2026-07-24 23:23:07 -0500

    Restructure microservices project architecture

--- apps/supplier-service/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { SupplierServiceModule } from './supplier-service.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(SupplierServiceModule);
-  await app.listen(process.env.port ?? 3000);
-}
-bootstrap();
